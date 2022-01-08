de0e6b05f8988a819f9ad5591ea1d0ca58c6681f
Ethan Riegsecker <ethanriegsecker1@gmail.com>
2022-01-07 23:50:33 -0600

    added day1 solution for python

--- Python/aoc_2021/day1/day1.py
+++ Python/aoc_2021/day1/day1.py
@@ -1,1 +1,13 @@
-print("Hello")
+input = "in.txt"
+
+ans1 = 0
+ans2 = 0
+XS = [int(x) for x in open(input)]
+for i in range(len(XS)):
+    if i >= 1 and XS[i] > XS[i-1]:
+        ans1 += 1
+    if i >= 3 and XS[i] > XS[i-3]:
+        ans2 += 1
+
+print("part 1 answer:", ans1)
+print("part 2 answer:", ans2)
